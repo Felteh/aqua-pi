521aca17a383146d418f0bc93a24f305fd7acd04
daniel.stoner <daniel.stoner@ocado.com>
2020-02-25 20:18:20 +0000

    Some basics

--- client/App.js
+++ client/App.js
@@ -1,15 +1,83 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import Button from '@material-ui/core/Button';
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import Toolbar from "@material-ui/core/Toolbar";
+import AppBar from "@material-ui/core/AppBar";
+import MenuIcon from '@material-ui/icons/Menu';
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import MoreIcon from '@material-ui/icons/MoreVert';
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+}));
 
 function App() {
- return (
-     <Button variant="contained" color="primary">
-       Hello World
-     </Button>
-   );
+    const classes = useStyles();
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleMenu = event => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <div>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <MenuIcon/>
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                        Aqua Pi
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                    <IconButton edge="end" color="inherit" onClick={handleMenu}>
+                        <MoreIcon/>
+                    </IconButton>
+                    <Menu
+                        id="menu-appbar"
+                        anchorEl={anchorEl}
+                        anchorOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        keepMounted
+                        transformOrigin={{
+                            vertical: 'top',
+                            horizontal: 'right',
+                        }}
+                        open={open}
+                        onClose={handleClose}
+                    >
+                        <MenuItem onClick={handleClose}>Reboot</MenuItem>
+                    </Menu>
+                </Toolbar>
+            </AppBar>
+            <Container maxWidth="md">
+                <Button variant="contained" color="primary">
+                    Hello World
+                </Button>
+            </Container>
+        </div>
+    );
 }
 
 export default App;
