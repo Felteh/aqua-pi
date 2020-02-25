8bb6bbb63bb6ed7003c9779f974ec04f04c1226c
Felteh <daniel.stoner@ocado.com>
2020-02-25 19:14:19 +0000

    Basic setup

---
+++ src/main/java/com/felteh/aquapi/AquapiApplication.java
@@ -0,0 +1,13 @@
+package com.felteh.aquapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felteh/aquapi/AquapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felteh.aquapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
